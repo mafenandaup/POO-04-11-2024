0b91554d300d60ac7957e6c81a4a8a50d99b8a94
Maria Fernanda Maia <88058637+mafenandaup@users.noreply.github.com>
2024-11-04 20:07:42 -0300

    Update Funcionario.java

--- Funcionario.java
+++ Funcionario.java
@@ -1,5 +1,19 @@
 package pooabstract;
 
-public class Funcionario {
+public abstract class Funcionario {
 
+	protected String nome;
+	protected double salario;
+	
+	public Funcionario(String nome, double salario) {
+		this.nome = nome;
+		this.salario = salario;
+	}
+	
+	public abstract String toString();
+	public abstract void getBonificacao();
+	
+	public void aumento() {
+		
+	}
 }
