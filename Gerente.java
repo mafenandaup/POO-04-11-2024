97dacadc478ce59c3389c302615921efc7762cd7
Maria Fernanda Maia <88058637+mafenandaup@users.noreply.github.com>
2024-11-04 21:10:09 -0300

    Update Gerente.java

--- Gerente.java
+++ Gerente.java
@@ -1,5 +1,27 @@
 package pooabstract;
 
-public class Gerente {
+public class Gerente extends Funcionario{
+	
+	protected int pessoas_gerenciadas;
 
+	public Gerente(String nome, double salario, int pessoas_gerenciadas) {
+		super(nome, salario);
+		this.pessoas_gerenciadas = pessoas_gerenciadas;
+	}
+
+	@Override
+	public String toString() {
+		return "GERENTE: nome:" +this.nome+ ". SALÁRIO: " +this.salario+ ". PESSOAS GERENCIADAS:" +pessoas_gerenciadas;
+	}
+
+	@Override
+	public double getBonificacao() {
+		return this.salario*0.20;
+	}
+
+	public int getPessoas() {
+		return pessoas_gerenciadas;
+		
+	}
+	
 }
