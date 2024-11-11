bb0628dd3f4b90bae0fe4241bc9c2e3f293b19b7
Maria Fernanda Maia <88058637+mafenandaup@users.noreply.github.com>
2024-11-11 07:56:32 -0300

    Update Conta.java

--- poorev/poorewiew/Conta.java
+++ poorev/poorewiew/Conta.java
@@ -7,34 +7,23 @@ public class Conta {
 	Scanner sc = new Scanner(System.in);
 	protected double saldo;
 
-
 	public Conta(double saldo) {
 		this.saldo = saldo;
 	}
-	public  String toString() {
-		return this.saldo+ " (saldo); " ;
-		
-	}
-	public double Deposito(double saldo) {
+
+	public void Deposito(double saldo) {
 		System.out.println("Quanto de cachê deseja adicionar");
 		double quantiadd = sc.nextDouble();
-		if(quantiadd > 0) {
-			saldo = saldo + quantiadd;
-			return saldo;
-		}
-		return saldo;
-
+			saldo =+ quantiadd;
 	}
 
-	public boolean Saque(double saldo) {
+	public void Saque(double saldo) {
 		System.out.println("Quanto de cachê deseja remover?");
 		double quantirem = sc.nextDouble();
 		if (quantirem <= this.saldo) {
-			saldo = saldo - quantirem;
-			return true;
+			saldo =- quantirem;
 		} else {
 			System.out.println("O seu saque é maior que a quantia de dinheiro em sua conta. tente novamente.");
-			return false;
 		}
 
 	}
