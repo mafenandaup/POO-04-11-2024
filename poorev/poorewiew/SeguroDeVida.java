21547ae170a775627a564eb5eab0b6dda65d0a12
Maria Fernanda Maia <88058637+mafenandaup@users.noreply.github.com>
2024-11-11 08:27:01 -0300

    Update SeguroDeVida.java

--- poorev/poorewiew/SeguroDeVida.java
+++ poorev/poorewiew/SeguroDeVida.java
@@ -1,6 +1,12 @@
 package pooreview;
 
 public class SeguroDeVida implements Tributavel {
+	
+	protected double valorSegurado;
+	
+	public void SeguroDeVuda(double vlr) {
+		this.valorSegurado = vlr;
+	}
 
 	@Override
 	public double calculaTributos() {
