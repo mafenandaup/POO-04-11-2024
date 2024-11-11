2dbeeb5783c4d3449f710c1b611662246ae74329
Maria Fernanda Maia <88058637+mafenandaup@users.noreply.github.com>
2024-11-11 16:51:49 -0300

    Update Freelancer.java

--- poorev/paymentsyst/Freelancer.java
+++ poorev/paymentsyst/Freelancer.java
@@ -1,16 +1,22 @@
 package paymentsyst;
 
 public class Freelancer extends Trabalhador{
+	
+	protected double valorproj;
 
 	public Freelancer(String nome, double salario) {
 		super(nome, salario);
-		// TODO Auto-generated constructor stub
+		this.salario = valorproj;
 	}
 
 	@Override
 	public double calcPayment() {
-		// TODO Auto-generated method stub
-		return 0;
+		return valorproj;
+	}
+	
+	public String toString(){
+		return ("NOME; " +nome+ ". VALOR DO PROJETO; R$"+valorproj);
+		
 	}
 
 }
