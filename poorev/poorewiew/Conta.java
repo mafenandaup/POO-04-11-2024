3418293b7a3de523378cd4ca904910941bcd8099
Maria Fernanda Maia <88058637+mafenandaup@users.noreply.github.com>
2024-11-11 08:14:32 -0300

    Update Conta.java

--- poorev/poorewiew/Conta.java
+++ poorev/poorewiew/Conta.java
@@ -4,28 +4,31 @@ import java.util.Scanner;
 
 public class Conta {
 
-	Scanner sc = new Scanner(System.in);
-	protected double saldo;
+    protected double saldo;
+    private Scanner sc = new Scanner(System.in);
 
-	public Conta(double saldo) {
-		this.saldo = saldo;
-	}
+    public Conta(double saldo) {
+        this.saldo = saldo;
+    }
 
-	public double Deposito(double saldo) {
-		System.out.println("Quanto de cachê deseja adicionar");
-		double quantiadd = sc.nextDouble();
-		return saldo = +quantiadd;
-	}
+    public double Deposito() {
+        System.out.println("Quanto de cachê deseja adicionar?");
+        double quantiadd = sc.nextDouble();
+        saldo += quantiadd;  // Corrigido o uso do +=
+        return saldo;
+    }
 
-	public double Saque(double saldo) {
-		System.out.println("Quanto de cachê deseja remover?");
-		double quantirem = sc.nextDouble();
-		if (quantirem <= this.saldo) {
-			return saldo = -quantirem;
-		} else {
-			System.out.println("O seu saque é maior que a quantia de dinheiro em sua conta. tente novamente.");
-		}
-		return saldo;
+    public double Saque() {
+        System.out.println("Quanto de cachê deseja remover?");
+        double quantirem = sc.nextDouble();
+        if (quantirem <= saldo) {
+            saldo -= quantirem; 
+        } else {
+            System.out.println("O seu saque é maior que a quantia de dinheiro em sua conta. Tente novamente.");
+        }
+        return saldo;
+    }
 
-	}
-}
+    public double getSaldo() {
+        return saldo;
+    }
