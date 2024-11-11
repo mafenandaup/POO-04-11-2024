5f0dba5a54f17a80534439261d18d81dc51ed312
Maria Fernanda Maia <88058637+mafenandaup@users.noreply.github.com>
2024-11-11 07:56:50 -0300

    Update ContaCorrente.java

--- poorev/poorewiew/ContaCorrente.java
+++ poorev/poorewiew/ContaCorrente.java
@@ -1,5 +1,14 @@
 package pooreview;
 
-public class ContaCorrente {
+public class ContaCorrente extends Conta implements Tributavel{
+
+	public ContaCorrente(double saldo) {
+		super(saldo);
+	}
+
+	@Override
+	public double calculaTributos() {
+		return saldo*0.01;
+	}
 
 }
