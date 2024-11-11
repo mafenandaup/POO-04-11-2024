e9f205da60a717bc196f1a626c2db7b123eb6b00
Maria Fernanda Maia <88058637+mafenandaup@users.noreply.github.com>
2024-11-11 16:52:34 -0300

    Update AppPayment.java

--- poorev/paymentsyst/AppPayment.java
+++ poorev/paymentsyst/AppPayment.java
@@ -3,6 +3,16 @@ package paymentsyst;
 public class AppPayment {
 
 	public static void main(String[] args) {
+		Funcionario func = new Funcionario ("Maria", 3000); 
+		Freelancer freela = new Freelancer("Jonas", 500);
+		Terceirizado pj = new Terceirizado ("Wilson" , 2000, 4);
+		
+		System.out.println("BOOKING DOS TRABALHADORES:");
+		System.out.println(" ");
+		System.out.println(func.toString());
+		System.out.println(freela.toString());
+		System.out.println(pj.toString());
+		System.out.println("SALÁRIO ATUALIZADO DO " +pj.getNome()+ "; "+pj.calcPayment());
 
 	}
 
