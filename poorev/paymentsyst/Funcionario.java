49d1dbb757121739358eee9c407cfef9b5732c73
Maria Fernanda Maia <88058637+mafenandaup@users.noreply.github.com>
2024-11-11 16:31:34 -0300

    Update Funcionario.java

--- poorev/paymentsyst/Funcionario.java
+++ poorev/paymentsyst/Funcionario.java
@@ -4,13 +4,11 @@ public class Funcionario extends Trabalhador{
 
 	public Funcionario(String nome, double salario) {
 		super(nome, salario);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public double calcPayment() {
-		// TODO Auto-generated method stub
-		return 0;
+		return salario;
 	}
 
 }
