068d6110d898de56c36d0d137234004edfac4329
Maria Fernanda Maia <88058637+mafenandaup@users.noreply.github.com>
2024-11-11 19:07:18 -0300

    Update TesteLoja.java

--- poorev/onlinestore/TesteLoja.java
+++ poorev/onlinestore/TesteLoja.java
@@ -3,8 +3,19 @@ package onlinestore;
 public class TesteLoja {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ProdutoDigital pd = new ProdutoDigital("E-book", 20.98);
+		ProdutoFisico pf = new ProdutoFisico("Caixa de ovos", 23.60);
+		ProdutoDigital pd2 = new ProdutoDigital("Processador", 53.38);
+		ProdutoFisico pf2 = new ProdutoFisico("Calculadora científica", 71.90);
+		ProdutoDigital pd3 = new ProdutoDigital("Chapinha", 87.38);
+		ProdutoFisico pf3 = new ProdutoFisico("Kiwi(1kg)", 14.49);
+		
+		System.out.println(pd.toString());
+		System.out.println(pf.toString());
+		System.out.println(pd2.toString());
+		System.out.println(pf2.toString());
+		System.out.println(pd3.toString());
+		System.out.println(pf3.toString());
 	}
 
 }
