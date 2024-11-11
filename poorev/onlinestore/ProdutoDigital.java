fe4985082fd5d850fb221975680cd597ca667876
Maria Fernanda Maia <88058637+mafenandaup@users.noreply.github.com>
2024-11-11 18:58:01 -0300

    Update ProdutoDigital.java

--- poorev/onlinestore/ProdutoDigital.java
+++ poorev/onlinestore/ProdutoDigital.java
@@ -4,6 +4,12 @@ public class ProdutoDigital extends Produto {
 
 	public ProdutoDigital(String nome, double preco) {
 		super(nome, preco);
+		
+	}
+	
+	public String toString(){
+		return ("NOME; " +nome+ ". PREÇO DO PRODUTO DIGITAL; R$"+preco);
+		
 	}
 
 }
