3a8be9f73cca6aef02f97488e473fdf095f8c597
Maria Fernanda Maia <88058637+mafenandaup@users.noreply.github.com>
2024-11-11 08:43:22 -0300

    Update ManageImpostosRenda.java

--- poorev/poorewiew/ManageImpostosRenda.java
+++ poorev/poorewiew/ManageImpostosRenda.java
@@ -3,4 +3,14 @@ package pooreview;
 public class ManageImpostosRenda {
 
 	private double total;
+
+	public void Add(Tributavel t) { // recebe um objeto tributavel T como parâmetro
+		this.total = +t.calculaTributos();
+
+	}
+
+	public double getTotal() {
+		return total;
+	}
+
 }
