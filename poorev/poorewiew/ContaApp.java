498eb84b6393bdc14d59e435bbebc22f8be0faf7
Maria Fernanda Maia <88058637+mafenandaup@users.noreply.github.com>
2024-11-11 08:30:30 -0300

    Update ContaApp.java

--- poorev/poorewiew/ContaApp.java
+++ poorev/poorewiew/ContaApp.java
@@ -4,11 +4,14 @@ public class ContaApp {
 
 	public static void main(String[] args) {
 		ContaCorrente contac = new ContaCorrente(5000);
+		ContaPoupanca contap = new 	ContaPoupanca(2000);
 		contac.Deposito();
-		SeguroDeVida segvida = new SeguroDeVida();
+		SeguroDeVida segvida = new SeguroDeVida(10000);
 		
+		System.out.println("SALDO CONTA CORRENTE; " + contac.getSaldo());
+		System.out.println("SALDO CONTA POUPANÇA; " + contap.getSaldo());
 		System.out.println("TRIBUTOS CONTA CORRENTE (R$): " + contac.calculaTributos());
-		System.out.println("TRIBUTOS CONTA CORRENTE (R$): " + segvida.calculaTributos());
+		System.out.println("TRIBUTOS SEGURO DE VIDA (R$): " + segvida.calculaTributos());
 
 	}
 
