62c96475704dd5797b64f2124c4b2ae8a0f55908
Maria Fernanda Maia <88058637+mafenandaup@users.noreply.github.com>
2024-11-18 07:58:29 -0300

    Update listexample.java

--- poocollections/src/testing/listexample.java
+++ poocollections/src/testing/listexample.java
@@ -2,8 +2,12 @@ package testing;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
 
 public class listexample {
@@ -25,17 +29,18 @@ public class listexample {
 		System.out.println(" ");
 		System.out.println("*ORDENADO");
 		System.out.println(" ");
-		
-		//ORDENAÇÃO COM LISTAS 
-		//A classe Collections fornece um método estático sort , que recebe um List como argumento e o
-		//ordena por ordem crescente.
-		
+
+		// ORDENAÇÃO COM LISTAS
+		// A classe Collections fornece um método estático sort , que recebe um List
+		// como argumento e o
+		// ordena por ordem crescente.
+
 		Collections.sort(lista);
-		for(int i=0; i < lista.size(); i++) {
-		nome = (String) lista.get(i);
-		System.out.println(nome);
+		for (int i = 0; i < lista.size(); i++) {
+			nome = (String) lista.get(i);
+			System.out.println(nome);
 		}
-		
+
 		// EXEMPLO COM SET (SAÍDA NÃO TEM ORDEM GARANTIDA)
 		Set<String> conjunto = new HashSet<>(); // não precisa fazer casting pq já define como string no inicio, com
 												// generics
@@ -47,6 +52,27 @@ public class listexample {
 		System.out.println("Conteúdo do do set: " + conjunto);
 		// A ordem dos elementos no HashSet não é previsível,
 		// mas pode ser preservada em outras implementações de Set, como LinkedHashSet.
-	}
-}
 
+		// EXEMPLO COM MAP: MAPEIA PARES CHAVE-VALOR
+
+		Map<Integer, String> mapa = new HashMap<>();
+		mapa.put(1, "Java");
+		mapa.put(2, "Python");
+		mapa.put(1, "C++"); // Substitui o valor associado à chave 1
+
+		System.out.println("Conteúdo do mapa: " + mapa);
+		System.out.println("Valor da chave 1: " + mapa.get(1)); // Acessa por chave
+
+// FILA PRIORITY QUEUE ; TRABALHA NO ESTILO FIRST IN, FIRST OUT (FIFO); 
+
+		Queue<String> fila = new LinkedList<>();
+		fila.add("Primeiro");
+		fila.add("Segundo");
+		fila.add("Terceiro");
+
+		System.out.println("Fila: " + fila);
+		System.out.println("Elemento removido: " + fila.poll()); // Remove o primeiro elemento add na fila
+		System.out.println("Fila atualizada: " + fila);
+	}
+
+}
