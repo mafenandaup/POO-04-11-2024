66d3b58899bf6ca8f9a171ac2697b3a143f5a81b
Maria Fernanda Maia <88058637+mafenandaup@users.noreply.github.com>
2024-11-25 16:08:50 -0300

    Update Peca.java

--- poo-trabalho2/src/clothingstore/Peca.java
+++ poo-trabalho2/src/clothingstore/Peca.java
@@ -2,4 +2,36 @@ package clothingstore;
 
 public class Peca {
 
+	private String descricao;
+	private int quantidade; 
+	private int estoque_min;
+	private int estoque_max;
+	
+	public Peca() {
+		
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public int getEstoque_min() {
+		return estoque_min;
+	}
+	public void setEstoque_min(int estoque_min) {
+		this.estoque_min = estoque_min;
+	}
+	public int getEstoque_max() {
+		return estoque_max;
+	}
+	public void setEstoque_max(int estoque_max) {
+		this.estoque_max = estoque_max;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
 }
