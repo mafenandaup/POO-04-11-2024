9982ecff797cd5f5c3ef69925fa13ae127943c7f
Maria Fernanda Maia <88058637+mafenandaup@users.noreply.github.com>
2024-11-25 16:23:23 -0300

    Update Acessorio.java

--- poo-trabalho2/src/clothingstore/Acessorio.java
+++ poo-trabalho2/src/clothingstore/Acessorio.java
@@ -1,5 +1,24 @@
 package clothingstore;
 
-public class Acessorio {
+import java.util.Scanner;
 
-}
+public class Acessorio extends Peca implements Item {
+
+	public Acessorio(String descricao, int quantidade, int estoque_min, int estoque_max) {
+		super(descricao, quantidade, estoque_min, estoque_max);
+	}
+
+	@Override
+	public void Venda() {
+		  Scanner sc = new Scanner(System.in);
+	        System.out.print("Digite a quantidade vendida de " + descricao + ": ");
+	        int quantidadeVendida = sc.nextInt();
+
+	        if (quantidadeVendida > quantidade) {
+	            System.out.println("Venda não realizada. Estoque insuficiente.");
+	        } else {
+	            quantidade -= quantidadeVendida;
+            System.out.println("Venda realizada. Estoque atual de " + descricao + ": " + quantidade+ " itens disponíveis.");
+	        }
+	    }
+	}
