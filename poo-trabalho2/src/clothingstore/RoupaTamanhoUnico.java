f31da93034f15cc948420dec73724831934d5628
Maria Fernanda Maia <88058637+mafenandaup@users.noreply.github.com>
2024-11-25 16:38:09 -0300

    Update RoupaTamanhoUnico.java

--- poo-trabalho2/src/clothingstore/RoupaTamanhoUnico.java
+++ poo-trabalho2/src/clothingstore/RoupaTamanhoUnico.java
@@ -1,5 +1,24 @@
 package clothingstore;
 
-public class RoupaTamanhoUnico {
+public class RoupaTamanhoUnico extends Peca implements Item {
 
+	public RoupaTamanhoUnico(String descricao, int quantidade, int estoque_min, int estoque_max) {
+		super(descricao, quantidade, estoque_min, estoque_max);
+	}
+
+	@Override
+	public void Venda() {
+		try {
+			if (quantidade < 0) {
+				throw new IllegalArgumentException("Venda não realizada. Estoque insuficiente.");
+			} else {
+				quantidade -= 1;
+				System.out.println("Venda realizada. Estoque atual de " + descricao + ": " + quantidade);
+			}
+		} catch (IllegalArgumentException e) {
+			System.out.println("Erro!: " + e.getMessage());
+		} catch (Exception e) {
+			System.out.println("Erro inesperado!: " + e.getMessage());
+		}
+	}
 }
