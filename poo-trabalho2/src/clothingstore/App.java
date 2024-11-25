5004de73811e7b7b6f9517c326a5a920d29bc37c
Maria Fernanda Maia <88058637+mafenandaup@users.noreply.github.com>
2024-11-25 17:22:28 -0300

    Update App.java

--- poo-trabalho2/src/clothingstore/App.java
+++ poo-trabalho2/src/clothingstore/App.java
@@ -3,8 +3,13 @@ package clothingstore;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		  Item[] itens = new Item[5];
 
+	        itens[0] = new Acessorio("Legging", 10, 2, 20); // descrição, quantia, estoque mínimo e estoqaue máximo
+	        itens[1] = new RoupaTamanhoUnico("Vestido", 5, 1, 10);
+	        itens[2] = new RoupaPMG("Camisa Polo", 5, 3, 2, 1, 15);
+	        itens[3] = new Acessorio("Pulseira", 8, 3, 20);
+	        itens[4] = new RoupaTamanhoUnico("Saída de Praia", 4, 1, 10);
 	}
 
 }
