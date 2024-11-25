02dd0dde8b561836d3df5a9dd9e49c8071bdf9d9
Maria Fernanda Maia <88058637+mafenandaup@users.noreply.github.com>
2024-11-25 16:14:54 -0300

    Update Peca.java

--- poo-trabalho2/src/clothingstore/Peca.java
+++ poo-trabalho2/src/clothingstore/Peca.java
@@ -7,11 +7,26 @@ public abstract class Peca {
 	private int estoque_min;
 	private int estoque_max;
 	
-	public Peca() {
-		
+	public Peca(String descricao, int quantidade, int estoque_min, int estoque_max) {
+		this.descricao = descricao;
+		this.quantidade = quantidade;
+		this.estoque_max = estoque_max;
+		this.estoque_min = estoque_min;
 	}
 	
+	public abstract void Venda();
 	
+	
+	   public void reposicaoEstoque() {
+	        if (quantidade < estoque_min) {
+	            quantidade = estoque_max;
+	            System.out.println(descricao + ": Estoque foi reposto ao máximo.");
+	        } else {
+	            System.out.println(descricao + ": Estoque não precisa de reposição.");
+	        }
+	    }
+	   
+	   
 	public String getDescricao() {
 		return descricao;
 	}
@@ -32,6 +47,12 @@ public abstract class Peca {
 	}
 	public int getQuantidade() {
 		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+}
+
 	}
 	public void setQuantidade(int quantidade) {
 		this.quantidade = quantidade;
