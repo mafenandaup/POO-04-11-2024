6d2e7be18a59d71cf3bbd3403e704034f767bbfc
Maria Fernanda Maia <88058637+mafenandaup@users.noreply.github.com>
2024-11-25 17:04:44 -0300

    Update RoupaPMG.java

--- poo-trabalho2/src/clothingstore/RoupaPMG.java
+++ poo-trabalho2/src/clothingstore/RoupaPMG.java
@@ -1,5 +1,19 @@
 package clothingstore;
 
-public class RoupaPMG {
+public class RoupaPMG implements Item {
 
+	protected String descricao;
+	private int quantidadeP;
+	private int quantidadeM;
+	private int quantidadeG;
+	protected int estoque_min;
+	protected int estoque_max;
+
+	public RoupaPMG(String descricao, int quantidadeP, int quantidadeM, int quantidadeG, int estoqueMinimo,
+			int estoqueMaximo) {
+
+		this.quantidadeP = quantidadeP;
+		this.quantidadeM = quantidadeM;
+		this.quantidadeG = quantidadeG;
+	}
 }
