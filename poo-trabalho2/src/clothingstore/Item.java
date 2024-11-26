e56094fcd5bef524711b5dfa7725696a4bdfa8d3
Maria Fernanda Maia <88058637+mafenandaup@users.noreply.github.com>
2024-11-25 21:13:55 -0300

    Update Item.java

--- poo-trabalho2/src/clothingstore/Item.java
+++ poo-trabalho2/src/clothingstore/Item.java
@@ -1,5 +1,8 @@
 package clothingstore;
 
 public interface Item {
-
+	    void Venda(); // Método para realizar a venda
+	    void reposicaoEstoque(); // Verifica e ajusta o estoque se necessário
+	    String getDescricao(); // Retorna a descrição do item
 }
+
